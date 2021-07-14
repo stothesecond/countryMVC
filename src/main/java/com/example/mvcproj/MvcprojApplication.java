c06578d4399d61c206f0cb2b8e361af4ce53fbf9
Steven Santos <stevensan0606@gmail.com>
2021-07-14 15:20:13 -0400

    added entry point web

--- src/main/java/com/example/mvcproj/MvcprojApplication.java
+++ src/main/java/com/example/mvcproj/MvcprojApplication.java
@@ -2,12 +2,20 @@ package com.example.mvcproj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MvcprojApplication {
+public class MvcprojApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
-    SpringApplication.run(MvcprojApplication.class, args);
+    SpringApplication.run(mvcprojApplicationClass, args);
   }
 
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(mvcprojApplicationClass);
+  }
+
+  private static Class<MvcprojApplication> mvcprojApplicationClass = MvcprojApplication.class;
 }
