5686715be0c878e99da818351500050f0c1b091c
Steven Santos <stevensan0606@gmail.com>
2021-07-14 16:09:40 -0400

    made thymeleaf pages of index and about pages

--- src/main/java/com/example/mvcproj/controller/AppController.java
+++ src/main/java/com/example/mvcproj/controller/AppController.java
@@ -1,17 +1,23 @@
 package com.example.mvcproj.controller;
 
-import com.example.mvcproj.model.Country;
 import com.example.mvcproj.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Optional;
 
 @Controller
 public class AppController {
   CountryService countryService;
+
+  @GetMapping
+  public String index() {
+    return "index";
+  }
+
+  @GetMapping
+  public String about() {
+    return "about";
+  }
 
   @PostMapping("/delete/{id}")
   public String deleteCountry(@PathVariable(name = "id") int countryId) {
