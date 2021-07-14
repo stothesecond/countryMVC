129979edf016f61ef471c08fe465879f04f78818
Steven Santos <stevensan0606@gmail.com>
2021-07-14 15:56:35 -0400

    separated servelet

--- src/main/java/com/example/mvcproj/MvcprojApplication.java
+++ src/main/java/com/example/mvcproj/MvcprojApplication.java
@@ -2,20 +2,12 @@ package com.example.mvcproj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class MvcprojApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
-    SpringApplication.run(mvcprojApplicationClass, args);
+    SpringApplication.run(MvcprojApplication.class, args);
   }
-
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-    return builder.sources(mvcprojApplicationClass);
-  }
-
-  private static Class<MvcprojApplication> mvcprojApplicationClass = MvcprojApplication.class;
 }
